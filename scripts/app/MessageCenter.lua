daa9fb063aea6687d93909c97142582e6c10c5fb
ivan <ivan_vigoss@qq.com>
2013-12-11 18:22:24 +0800

    Global Message Center

--- scripts/app/MessageCenter.lua
+++ scripts/app/MessageCenter.lua
@@ -5,10 +5,11 @@
 --
 
 --3rd socket tool
-local socket = import(".net.SocketTCP")
+local SocketTCP = import(".net.SocketTCP")
 
-local MessageCenter = class("MessageCenter", socket)
+local MessageCenter = class("MessageCenter")
 local ByteArray  = import(".utils.ByteArray")
+local scheduler = require(cc.PACKAGE_NAME .. ".scheduler")
 
 --定义Model需要监听此处定义的事件，然后再分发
 --e.g addEventListener("ON_ENTER_ROOM_EVENT",handler)
@@ -17,10 +18,10 @@ MessageCenter.EVENTS = {
 }
 
 --定义服务端的接口
-MessageCenter.ENTER_ROOM_SERVICE = 1
-MessageCenter.LEFT_ROOM_SERVICE = 2
-MessageCenter.ON_READY_SERVICE = 3
-MessageCenter.ANSWER_SERVICE = 4
+ENTER_ROOM_SERVICE = 1
+LEFT_ROOM_SERVICE = 2
+ON_READY_SERVICE = 3
+ANSWER_SERVICE = 4
 
 --具体的服务接口，保持与上面定义的SERVICE的顺序一致
 MessageCenter.SERVICES = {
@@ -31,20 +32,34 @@ MessageCenter.SERVICES = {
 }
 
 
-function MessageCenter:ctor(host,post,retryWhenConnnectFailed)
-	MessageCenter.super:ctor(host,post,retryWhenConnnectFailed)
-	self:addEventListener(socket.EVENT_CONNECTED, handler(self, self.onStatus))
-	self:addEventListener(socket.EVENT_CLOSE, handler(self,self.onStatus))
-	self:addEventListener(socket.EVENT_CLOSED, handler(self,self.onStatus))
-	self:addEventListener(socket.EVENT_CONNECT_FAILURE, handler(self,self.onStatus))
-	self:addEventListener(socket.EVENT_DATA, handler(self,self.onData))
-	MessageCenter.super:connect()
+function MessageCenter:ctor()
+	--support event
+	cc.GameObject.extend(self)
+    self:addComponent("components.behavior.EventProtocol"):exportMethods()
+    --todo http way get host & port
+    if not self.socket_ then
+		self.socket_ = SocketTCP.new("127.0.0.1",9110,true)
+		--add event
+		self.socket_:addEventListener(SocketTCP.EVENT_CONNECTED, handler(self, self.onStatus))
+		self.socket_:addEventListener(SocketTCP.EVENT_CLOSE, handler(self,self.onStatus))
+		self.socket_:addEventListener(SocketTCP.EVENT_CLOSED, handler(self,self.onStatus))
+		self.socket_:addEventListener(SocketTCP.EVENT_CONNECT_FAILURE, handler(self,self.onStatus))
+		self.socket_:addEventListener(SocketTCP.EVENT_DATA, handler(self,self.onData))
+	end
+
+	if not self.socket_.isConnected then
+		self.socket_:connect()
+	end
+	--set global
+	sockettcp = self
 end
 
+function MessageCenter:isConnected()
+	return self.socket_.isConnected
+end
 
 function MessageCenter:onStatus(__event)
 	echoInfo("socket status: %s", __event.name)
-	echoInfo("isConnected: %s" , MessageCenter.super.isConnected)
 end
 
 function MessageCenter:onData(__event)
@@ -64,10 +79,10 @@ function MessageCenter:onData(__event)
 	end
 end
 
-
 --serviceCode  values must be one of MessageCenter.ENTER_ROOM_SERVICE,MessageCenter.LEFT_ROOM_SERVICE etc.
 --data to be send to server
 function MessageCenter:sendMessage(serviceCode,data)
+
 	assert(type(serviceCode) == "number","Invalid type, must be number")
 	assert(type(data) == "table","Invalid type,must be table")
 
@@ -82,7 +97,7 @@ function MessageCenter:sendMessage(serviceCode,data)
 	_ba:writeStringBytes(serviceName)
 	_ba:writeStringBytes(dataJson)
 
-	MessageCenter.super:send(_ba:getPack())
+	self.socket_:send(_ba:getPack())
 
 end
 
