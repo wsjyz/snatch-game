a08eb891a71eeaee982099361aa5338564c6fbef
ivan <ivan_vigoss@qq.com>
2013-12-11 18:22:54 +0800

    message center call

--- scripts/app/scenes/ChooseLevelScene.lua
+++ scripts/app/scenes/ChooseLevelScene.lua
@@ -1,7 +1,9 @@
+-- global variable sockettcp
+import("..MessageCenter").new()
 
 local CommonBackground = import("..views.CommonBackground")
-local MessageCenter = import("..MessageCenter").new("127.0.0.1",9110)
 local SettingMenu = import("..views.SettingMenu")
+
 local ChooseLevelScene = class("ChooseLevelScene",function ()
 	return display.newScene("ChooseLevelScene")
 end)
@@ -12,6 +14,10 @@ function ChooseLevelScene:ctor()
 	local LEVEL_OFFSET_Y = 50
 	local scale_rate = 1.1
 
+	cc.EventProxy.new(sockettcp , self)
+	:addEventListener("ON_ENTER_ROOM_EVENT",self.onEnterRoom,self)
+
+
 	CommonBackground.new():addTo(self)
 	--乡试
 	local easy = cc.ui.UIPushButton.new({
@@ -20,12 +26,13 @@ function ChooseLevelScene:ctor()
     		disabled = "#xiangshi.png"
 		})
 		:onButtonClicked(function(e) 
-			MessageCenter:sendMessage(MessageCenter.ENTER_ROOM_SERVICE, {
-					userId = "ivan",
-					nickName = "蒋凌峰",
+			sockettcp:sendMessage(ENTER_ROOM_SERVICE, {
+					userId = "ivan2",
+					nickName = "Ivan",
 					awardId = "ROOM110",
 					male = 1
 				})
+
 			end)
 		:onButtonPressed(function (e) 
 			 e.target:setScale(scale_rate)
@@ -77,6 +84,9 @@ function ChooseLevelScene:ctor()
 
 end
 
-
+function ChooseLevelScene:onEnterRoom(event)
+	echoInfo("ChooseLevelScene event receive: %s\n , data : %s", event.name , event.data)
+	app:enterPlayerWaiting()
+end
 
 return ChooseLevelScene
