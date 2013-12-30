a71fd02d86c9824b1ce7871a01d1bcf74310374a
ivan <ivan_vigoss@qq.com>
2013-12-30 18:52:59 +0800

    add music and sound

--- scripts/app/MyApp.lua
+++ scripts/app/MyApp.lua
@@ -6,6 +6,7 @@ require("framework.shortcodes")
 require("framework.cc.init")
 
 local MyApp = class("MyApp", cc.mvc.AppBase)
+local HttpClient = import(".HttpClient")
 
 function MyApp:ctor()
     MyApp.super.ctor(self)
@@ -13,23 +14,59 @@ function MyApp:ctor()
     --these values will be update in special scene
     self.me = {} --me , update when login
     self.currentLevel = 1 -- room level , default is 1
-    self.currentRoomId = nil -- udpate when enter room or quick start
+    self.currentAward = nil -- udpate when enter award room or  quick start
+    self.currentRoomId = nil -- room id 
+    self.topicList = {} -- random topicList
 end
 
 function MyApp:run()
     CCFileUtils:sharedFileUtils():addSearchPath("res/")
     display.addSpriteFramesWithFile(GAME_TEXTURE_DATA_FILENAME, GAME_TEXTURE_IMAGE_FILENAME)
     
-    self:enterScene("LoginScene")
+
+    -- preload all music
+    for k, v in pairs(GAME_MUSIC) do
+        audio.preloadMusic(v)
+    end
+    -- preload all sounds
+    for k, v in pairs(GAME_SOUND) do
+        audio.preloadSound(v)
+    end
+
+    --check weather exists in server
+    HttpClient.new(function(player) 
+        if player then
+            self.me = player
+            self:enterChooseLevel()
+        else
+            self:enterLoginScene()
+        end
+    end,getUrl(PLAYER_INFO_URL, device.getOpenUDID()))
+    :start()
+
+end
+
+function MyApp:loadTopicList(callback)
+    HttpClient.new(function(topicList) 
+        printf("load topicList ,as follows : %s", json.encode(topicList))
+        self.topicList = topicList
+        if callback then callback() end
+    end ,getUrl(TOPIC_LIST_URL, self.currentLevel)):start()
 end
 
 -- scene transition
+function MyApp:enterLoginScene()
+   self:enterScene("LoginScene") 
+end
+
 function MyApp:enterChooseLevel()
+    audio.playMusic(GAME_MUSIC.hall)
     self.currentLevel = 1  -- default
 	self:enterScene("ChooseLevelScene")
 end
 
 function MyApp:enterChooseAward(level)
+    audio.playMusic(GAME_MUSIC.game)
     self.currentLevel = level
 	local args = {level}
 	self:enterScene("RoomListScene", args)
