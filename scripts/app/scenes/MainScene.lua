93fd41a0645b8025c713e1acd5c8e411f1c8915f
ivan <ivan_vigoss@qq.com>
2013-12-30 18:56:25 +0800

    add welcome page

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -1,16 +1,17 @@
 
+local HttpClient = import("..HttpClient")
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
 function MainScene:ctor()
-    self.bg = display.newSprite("#bg.png", display.cx, display.cy)
-    self:addChild(self.bg)
+    app:createView("CommonBackground"):addTo(self)
 
     self.startBtn = ui.newImageMenuItem({
             image = "#logobtn.png",
             imageSelected = "#logobtn.png",
-            imageDisabled = "#logobtn.png"
+            imageDisabled = "#logobtn.png",
+            listener = handler(self, self.checkPlayer())
         })   
     self.startBtn:setPosition(display.cx,display.top - self.startBtn:getContentSize().height/2)
     self:addChild(self.startBtn)
@@ -18,8 +19,21 @@ function MainScene:ctor()
         time = 0.6,
         y = display.cy,
         easing = "BOUNCEOUT"
-        })
+    })
     
+end
+
+function MainScene:checkPlayer()
+    HttpClient.new(function(player) 
+        printf("player info %s", json.encode(player))
+        if player then
+            app.me = player
+            app:enterChooseLevel()
+        else
+            app:enterLoginScene()
+        end
+    end,getUrl(PLAYER_INFO_URL, device.getOpenUDID()))
+    :start()
 end
 
 function MainScene:onEnter()
