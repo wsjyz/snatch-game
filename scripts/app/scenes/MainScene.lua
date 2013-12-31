a1a2458ac22f25bbd040ee9a8af5b7ccd036ca91
ivan <ivan_vigoss@qq.com>
2013-12-31 17:07:22 +0800

    MainScene update

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -7,36 +7,38 @@ end)
 function MainScene:ctor()
     app:createView("CommonBackground"):addTo(self)
 
-    self.startBtn = ui.newImageMenuItem({
-            image = "#logobtn.png",
-            imageSelected = "#logobtn.png",
-            imageDisabled = "#logobtn.png",
-            listener = handler(self, self.checkPlayer())
-        })   
+    self.startBtn = cc.ui.UIPushButton.new("#start.png"):onButtonClicked(function(e) 
+            audio.playSound(GAME_SOUND["tapButton"])
+            self:checkPlayer()
+        end)
     self.startBtn:setPosition(display.cx,display.top - self.startBtn:getContentSize().height/2)
     self:addChild(self.startBtn)
     transition.moveTo(self.startBtn,{
         time = 0.6,
-        y = display.cy,
+        y = display.cy + 30,
         easing = "BOUNCEOUT"
     })
-    
+        
+    display.newSprite("#desk1.png", display.cx, display.bottom + 140):addTo(self)
+    display.newSprite("#people.png", display.right - 200, display.cy):addTo(self)
+
 end
 
 function MainScene:checkPlayer()
     HttpClient.new(function(player) 
-        printf("player info %s", json.encode(player))
         if player then
             app.me = player
             app:enterChooseLevel()
         else
-            app:enterLoginScene()
+            self.startBtn:removeSelf(true)
+            app:createView("LoginForm"):addTo(self,1)
         end
     end,getUrl(PLAYER_INFO_URL, device.getOpenUDID()))
     :start()
 end
 
 function MainScene:onEnter()
+    audio.playMusic(GAME_MUSIC.hall)
     if device.platform == "android" then
         -- avoid unmeant back
         self:performWithDelay(function()
